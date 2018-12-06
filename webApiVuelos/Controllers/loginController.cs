4c7219ac1eb417218b56bca867d54a4e3aeeff16
David1997sb <salasbar97@gmail.com>
2018-12-05 20:25:17 -0600

    Adding post methods to API

---
+++ webApiVuelos/Controllers/loginController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using webApiVuelos.Models;
+
+namespace webApiVuelos.Controllers
+{
+    public class loginController : ApiController
+    {
+
+        User[] usr = new User[]
+        {
+            new User{userName="dsalas", password="admin"}
+        };
+
+        [Route("api/login/getUserAuthentication")]
+        public IHttpActionResult getUserAuthentication(User userr)
+        {
+            var user = usr.FirstOrDefault((p) => p.userName == userr.userName && p.password==userr.password);
+            if (user.userName == null || user.password==null)
+            {
+                return NotFound();
+            }
+            return Ok(user);
+        }
+        [Route("api/login/GetAllUsers")]
+        [HttpGet]
+        public IEnumerable<User> GetAllUsers()
+        {
+            return usr;
+        }
+    }
+}
