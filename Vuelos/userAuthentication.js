a6111a207b5e65b546677c15e4cfd33d09151bb3
David1997sb <salasbar97@gmail.com>
2018-12-05 23:22:36 -0600

    merge

---
+++ Vuelos/userAuthentication.js
@@ -0,0 +1,21 @@
+﻿function postLogin() {
+    $.ajax({
+        url: "api/login/getUserAuthentication",
+        type: "POST",
+        data: { apiKey: "23462", method: "POST", ip: "208.74.35.5" },
+        dataType: "json",
+        success: function (result) {
+            switch (result) {
+                case true:
+                    processResponse(result);
+                    break;
+                default:
+                    resultDiv.html(result);
+            }
+        },
+        error: function (xhr, ajaxOptions, thrownError) {
+            alert(xhr.status);
+            alert(thrownError);
+        }
+    });
+}
